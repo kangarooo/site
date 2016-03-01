ac2bd6687cebca4b1a04232a2a8997c595f2cf43
jankos <janis.koselevs@gmail.com>
2016-03-01 16:16:25 +0200

    Updating for node

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
 });
