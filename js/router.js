9721cea27284715bf92bd7b9512d1d04bfce19a5
jankos <janis.koselevs@gmail.com>
2016-03-01 13:57:33 +0200

    New routes

--- js/router.js
+++ js/router.js
@@ -2,6 +2,8 @@ define(['jquery', 'underscore', 'backbone', 'views/default'],
 function ($, _, Backbone, DefaultView) {
   var SiteRouter = Backbone.Router.extend({
     routes: {
+      'about': 'aboutAction',
+      'contact': 'contactAction',
       '*other': 'defaultAction'
     }
   });
@@ -10,7 +12,12 @@ function ($, _, Backbone, DefaultView) {
     siteRouter.on('route:defaultAction', function(){
       var defaultView = new DefaultView();
       defaultView.render();
-      console.log('default');
+    });
+    siteRouter.on('route:aboutAction', function(){
+      console.log('about');
+    });
+    siteRouter.on('route:contactAction', function(){
+      console.log('contact');
     });
     Backbone.history.start();
   };
